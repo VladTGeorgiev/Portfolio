ab5af6b4635447be879d55ae502f9b0fb008983f
VladTGeorgiev <vlad.georgiev@outlook.com>
2019-11-04 23:30:31 +0000

    Contact form scaffold added

--- script.js
+++ script.js
@@ -1,27 +1,21 @@
-let mainNavLinks = document.querySelectorAll("nav ul li a");
-let mainSections = document.querySelectorAll("main section");
+let mainNavLinks = document.querySelectorAll('nav ul li a');
+let mainSections = document.querySelectorAll('main section');
 
 let lastId;
 let cur = [];
 
-window.addEventListener("scroll", event => {
-  let fromTop = window.scrollY;
+window.addEventListener('scroll', (event) => {
+	let fromTop = window.scrollY;
 
-  mainNavLinks.forEach(link => {
-    let section = document.querySelector(link.hash);
+	mainNavLinks.forEach((link) => {
+		let section = document.querySelector(link.hash);
 
-    if (
-      section.offsetTop <= fromTop &&
-      section.offsetTop + section.offsetHeight > fromTop
-    ) {
-      link.classList.add("current");
-      link.classList.add("tracking-in-contract");
-    } else {
-      link.classList.remove("current");
-      link.classList.remove("tracking-in-contract");
-    }
-  });
+		if (section.offsetTop <= fromTop && section.offsetTop + section.offsetHeight > fromTop) {
+			link.classList.add('current');
+			link.classList.add('tracking-in-contract');
+		} else {
+			link.classList.remove('current');
+			link.classList.remove('tracking-in-contract');
+		}
+	});
 });
-
-
-
